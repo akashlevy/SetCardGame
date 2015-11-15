832111a2923acfef24f234118fe68a18c881d292
Akash Levy <akashlevy@gmail.com>
2015-11-15 15:46:07 -0500

    Will Facebook ever work?

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ var sys = require('sys');
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 80));
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
   sys.print("running");
+});
+
+app.post('/', function(req, res) {
+    res.sendfile(__dirname + '/public/index.html');
 });
