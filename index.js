aeda49ff6cbfe14bf0e35cf962f6a24ceeac1095
Akash Levy <akashlevy@gmail.com>
2015-11-15 14:52:01 -0500

    Updated more facebook things

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 //var FB = require('fb');
 var sys = require('sys');
 var express = require('express');
+require('faceplate').middleware({
+    app_id: process.env.FACEBOOK_APP_ID,
+    secret: process.env.FACEBOOK_SECRET,
+    scope:  'user_likes,user_photos,user_photo_video_tags'
+  });
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
